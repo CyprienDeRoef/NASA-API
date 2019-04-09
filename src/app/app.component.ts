dd44a2ba63868416b0cefb02e39f249010c46dac
BLOT Martial <martial.blot@outlook.fr>
2019-04-09 16:01:02 +0200

    NasaApiWorks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NasaService } from './nasa.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public imgOfTheDay = '';
   title = 'NasaService';
+
+  public service:NasaService;
+
+    constructor( param_service:NasaService ){
+        this.service  =  param_service;
+    }
+
+    public  ngOnInit():void{
+        this.service.getImageOfTheDay().subscribe(
+            (param_img:string) => {
+                this.imgOfTheDay  =  param_img;
+                // console.log(param_img.url)
+                
+            }
+        );
+    }
 }
+
